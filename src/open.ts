0a50dc3195c3d99514fa8a6c470058b1bc2e91b3
Andrei Mackenzie <andreimackenzie@gmail.com>
2023-12-16 14:24:33 -0500

    Fix eslint errors and get a basic test working.

--- src/open.ts
+++ src/open.ts
@@ -1,28 +1,37 @@
 import * as vscode from 'vscode';
 import { Selection } from './editor';
 import { GitInfo } from './git';
-const GitUrlParse = require("git-url-parse");
+import  GitUrlParse from 'git-url-parse';
 
 export function open(gitInfo: GitInfo, selection: Selection ) {
-    const url = gitUrlToWebUrl(gitInfo, selection);
+    const parsed = GitUrlParse(gitInfo.originUri);
+    const url = gitUrlToWebUrl(parsed, gitInfo.commitHash, selection);
     vscode.env.openExternal(vscode.Uri.parse(url));
 }
 
-function gitUrlToWebUrl(gitInfo: GitInfo, selection: Selection): string {
-    const parsed = GitUrlParse(gitInfo.originUri);
-    const host = parsed.resource;
+class UrlParsed {
+    resource: string
+    pathname: string
+    constructor(resource: string, pathname: string) {
+        this.resource = resource;
+        this.pathname = pathname;
+    }
+}
+
+function gitUrlToWebUrl(url: UrlParsed, commitHash: string, selection: Selection): string {
+    const host = url.resource;
     const fragment = getLineNumberFragment(selection);
 
-    const path = parsed.pathname;
+    const path = url.pathname;
     const pathSplit = path.split('/');
     if (pathSplit.length === 2) {
         const project = pathSplit[0];
         const repo = pathSplit[1];
-        return `https://${host}/projects/${project}/repos/${repo}/browse/${selection.filePath}?at=${gitInfo.commitHash}${fragment}`
+        return `https://${host}/projects/${project}/repos/${repo}/browse/${selection.filePath}?at=${commitHash}${fragment}`
     }
 
     //TODO: make this work beyond stash
-    return `https://${host}/${path}/${selection.filePath}?at=${gitInfo.commitHash}${fragment}`
+    return `https://${host}/${path}/${selection.filePath}?at=${commitHash}${fragment}`
 }
 
 function getLineNumberFragment(selection: Selection): string {
---
+++ src/test/open.test.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert';
+
+suite('gitUrlToWebUrl', () => {
+	test('Sample test', () => {
+		assert.strictEqual(0, 0);
+	});
+});
